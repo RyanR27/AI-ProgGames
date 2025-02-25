d87a2d04f2e69387ac912288ca1052162fae735e
MaryOdu <146565129+MaryOdu@users.noreply.github.com>
2025-02-25 16:07:44 +0000

    task 2,3.5

--- assignment-pathfinderWORKS/src/pathfinder.cpp
+++ assignment-pathfinderWORKS/src/pathfinder.cpp
@@ -8,11 +8,9 @@
 #include <string>
 #include <unordered_map>
 
-//Task 1 
+//Task 1 Mary
 #include "raylib.h"
 
-//Task 12
-//#include <windows.h>
 
 std::vector<node_t> astar_pathfind(const Graph& g, node_t start, node_t goal)
 {
@@ -65,37 +63,77 @@ int main()
   add_double_edge(g, 'C', 'G');
   add_double_edge(g, 'F', 'G');
 
-  //Task 12
-  int t{60}; // time
-  /*for (t = 60; t >= 1; t - 1) {
+  //Task 12 Mary
+  /* int t{60};
+  for (t = 60; t >= 1; t - 1) {
       std::cout << t;
       sleep(60);
   }*/
+
+  float t{60}; // time from int to flaot William
 
   std::vector<node_t> player_path{};
   node_t start = 'A';
   node_t end   = 'G';
   int tokens{2000}, score{}, high_score{}; // try with more/less tokens?
 
+  //std::vector<node_t> path;
+  player_path.push_back(start); //Task 5 William
+
+
   while (!window.ShouldClose()) // Detect window close button or ESC key
   {
     BeginDrawing();
 
+    //William
+    t -= GetFrameTime();
+
     ClearBackground(LIGHTGRAY);
+
+   
+    //Task 5 William
+    if (player_path.size() >= 2)
+    {
+        for (int i = 1; i < player_path.size(); i++)
+        {
+            const node_t n1 = player_path[i - 1];
+            const node_t n2 = player_path[i];
+            const coord_t& coord_n1 = node_info[n1];
+            const coord_t& coord_n2 = node_info[n2];
+            DrawLineEx(coord_n1, coord_n2, line_thickness * 2, YELLOW); // William - Drawing yellow line using player path coordinates
+        }
+    }
 
     draw_graph(g);
 
     //Task 1
-    DrawText(TextFormat("Score: %04i", score), 100, 80, 20, WHITE);
-    DrawText(TextFormat("Tokens: %04i", tokens), 100, 120, 20, WHITE);
-    DrawText(TextFormat("High Score: %04i", high_score), 100, 160, 20, WHITE);
-    DrawText(TextFormat("Timer(s): %02i", t), 100, 200, 20, WHITE);
+    DrawText(TextFormat("Score: %04i", score), 100, 80, 20, WHITE); // Mary 
+    DrawText(TextFormat("Tokens: %04i", tokens), 100, 120, 20, WHITE); // Mary
+    DrawText(TextFormat("High Score: %04i", high_score), 100, 160, 20, WHITE); // Mary
+    DrawText(TextFormat("Timer(s): %02i", static_cast<int>(t)), 100, 200, 20, WHITE); //Mary and cast as int was Willam
+
+    //Task 2
+    DrawCircle(node_info[start].x, node_info[start].y, 12, GREEN); //William - Drawing green circle using start node position
+    DrawCircle(node_info[end].x, node_info[end].y, 12, RED); //William - Drawing red circle using end node position
 
     if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
     {
       if (auto opt = get_nearby_node(GetMousePosition()))
       {
         // *opt is a node_t
+         // Willam
+          auto nv = g.neighbors(player_path.back());
+
+          for (auto currNeighbour : nv)
+          {
+              if (currNeighbour == *opt)
+              {
+                  tokens -= g.cost(player_path.back(), *opt);
+                  player_path.push_back(*opt);
+                  //tokens -= path_cost(player_path); *maybe use it later -- final node
+                  break;
+              }
+          }
       }
     }
 
